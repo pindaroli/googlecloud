70e4b3a56ee3889c534c1047d1cc994a4bb63597
olindo.pindaro <copilot004@grigroup.com>
2024-04-22 08:53:36 +0200

    ok

---
+++ challange1.sh
@@ -0,0 +1,33 @@
+R=us-east1
+export ZONE=us-west1-c
+export PRJ=qwiklabs-gcp-04-05805755962a
+export BCK=$PRJ-olibk
+export STURL=https://storage.cloud.google.com/qwiklabs-gcp-04-05805755962a-olibk/install-web.sh
+
+cat << EOF > install-web.sh
+#!/bin/bash
+      apt-get update
+      apt-get install apache2 -y
+      service apache2 restart
+      echo "
+<h3>Web Server: www1</h3>" | tee /var/www/html/index.html
+EOF
+gsutil mb gs://$BCK/
+gsutil cp install-web.sh gs://$BCK/
+
+gcloud compute firewall-rules create www-firewall-network-lb \
+    --target-tags network-lb-tag --allow tcp:80
+
+
+
+  gcloud compute instances create www \
+    --zone=$ZONE \
+    --tags=network-lb-tag \
+    --machine-type=e2-small \
+    --image-family=debian-11 \
+    --image-project=debian-cloud \
+    --scopes storage-ro \
+    --metadata=startup-script-url=https://storage.googleapis.com/$BCK/install-web.s
+
+gcloud compute instances list
+curl http://[IP_ADDRESS]
